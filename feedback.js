a276bde45b3fd35df1f2d7bebaeed83ba33c4b30
Deepak Parpyani <dparpyani@yahoo.com>
2013-02-16 18:54:11 -0500

    Simplified feedback.js

--- feedback.js
+++ feedback.js
@@ -2,13 +2,7 @@
   feedback.js: Provides feedback to the user like help messages, errors, etc.
 */
 
-module.exports = (function feedback () {
-  function errorFeedback (message) {
-    console.log("Taskline: [error] " + message);
-    process.exit(1);
-  }
-  
-  return {
-    error: errorFeedback
-  };
-} ());
+module.exports.error = function (message) {
+  console.log("Taskline: [error] " + message);
+  process.exit(1);
+};
