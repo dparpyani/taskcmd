a63150a73ff71a8317eaffc4741e363bce47938e
Deepak Parpyani <dparpyani@yahoo.com>
2013-02-17 13:25:50 -0500

    Added ability to edit and prioritize tasks.

--- taskcmd.js
+++ taskcmd.js
@@ -14,6 +14,7 @@ var parameters = process.argv.slice(3);
 var aliases = require(__dirname + '/lib/aliases.json');
 
 // List tasks
+// usage: list option [keyword]
 if (aliases.list.indexOf(command) > -1) {
   if (parameters.length <= 2) {
     taskOperations.list(parameters[0], parameters[1]);
@@ -23,6 +24,7 @@ if (aliases.list.indexOf(command) > -1) {
 }
 
 // Add task
+// usage: add description
 if (aliases.add.indexOf(command) > -1) {
   if (parameters.length == 1) {
     taskOperations.add(parameters[0]);
@@ -32,6 +34,7 @@ if (aliases.add.indexOf(command) > -1) {
 }
 
 // Complete task
+// usage: complete id [note]
 if (aliases.complete.indexOf(command) > -1) {
   if (parameters.length <= 2) {
     taskOperations.complete(parameters[0], parameters[1]);
@@ -41,11 +44,32 @@ if (aliases.complete.indexOf(command) > -1) {
 }
 
 // Remove tasks
+// usage: remove option [keyword]
 if (aliases.remove.indexOf(command) > -1) {
   if (parameters.length <= 2) {
     taskOperations.remove(parameters[0], parameters[1]);
   } else {
     feedback.error('Too many arguments provided.');
+  }
+}
+
+// Edit task
+// usage: edit id option value
+if (aliases.edit.indexOf(command) > -1) {
+  if (parameters.length == 3) {
+    taskOperations.edit(parameters[0], parameters[1], parameters[2]);
+  } else {
+    feedback.error('Invalid number of arguments provided.');
+  }
+}
+
+// Prioritize task
+// usage: priority id value
+if (aliases.priority.indexOf(command) > -1) {
+  if (parameters.length == 2) {
+    taskOperations.edit(parameters[0], '-p', parameters[1]);
+  } else {
+    feedback.error('Invalid number of arguments provided.');
   }
 }
 
